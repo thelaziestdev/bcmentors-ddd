18709ee308daf1e0c1152d9a20c13b10932c090f
michalkoszalka <koszalkamichal1993@gmail.com>
2025-01-06 18:49:33 +0100

    first commit

---
+++ src/main/java/com/workshop/bcmentorsddd/BcmentorsDddApplication.java
@@ -0,0 +1,13 @@
+package com.workshop.bcmentorsddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BcmentorsDddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BcmentorsDddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/workshop/bcmentorsddd/BcmentorsDddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop.bcmentorsddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BcmentorsDddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
